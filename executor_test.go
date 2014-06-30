28cc5f5911c73dcb54bae6c585b5ff5d449b5cac
John Wregglesworth <wregglej@iplantcollaborative.org>
2014-06-30 13:39:01 -0700

    Moved the Jobs tests into their own file.

--- executor_test.go
+++ executor_test.go
@@ -4,9 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"os/exec"
-	"reflect"
-
-	"code.google.com/p/go-uuid/uuid"
 
 	"testing"
 	"time"
@@ -17,129 +14,6 @@ func TestExitCode(t *testing.T) {
 	cmd.Start()
 	cmd.Wait()
 	if exitCode(cmd) != 0 {
-		t.Fail()
-	}
-}
-
-func TestJobKilled(t *testing.T) {
-	j := NewBashCommand()
-	if j.Killed() {
-		t.Fail()
-	}
-}
-
-func TestJobSetKilled(t *testing.T) {
-	j := NewBashCommand()
-	j.SetKilled(true)
-	if !j.Killed() {
-		t.Fail()
-	}
-}
-
-func TestGetExitCode(t *testing.T) {
-	j := NewBashCommand()
-	if j.ExitCode() != -9000 {
-		t.Fail()
-	}
-}
-
-func TestSetExitCode(t *testing.T) {
-	j := NewBashCommand()
-	j.SetExitCode(1)
-	if j.ExitCode() != 1 {
-		t.Fail()
-	}
-}
-
-func TestCmdPtr(t *testing.T) {
-	j := NewBashCommand()
-	if j.CmdPtr() != nil {
-		t.Fail()
-	}
-}
-
-func TestSetCmdPtr(t *testing.T) {
-	j := NewBashCommand()
-	c := exec.Command("echo", "true")
-	j.SetCmdPtr(c)
-	if j.CmdPtr() != c {
-		t.Fail()
-	}
-}
-
-func TestUUID(t *testing.T) {
-	j := NewBashCommand()
-	if j.UUID() != "" {
-		t.Fail()
-	}
-}
-
-func TestSetUUID(t *testing.T) {
-	j := NewBashCommand()
-	u := uuid.New()
-	j.SetUUID(u)
-	if j.UUID() != u {
-		t.Fail()
-	}
-}
-
-func TestPrepare(t *testing.T) {
-	j := NewBashCommand()
-	var c string
-	var e map[string]string
-	go func() {
-		c = <-j.command
-		e = <-j.environment
-		<-j.begin
-		j.began <- 1
-	}()
-	j.Prepare("foobar", map[string]string{"foo": "bar"})
-	<-j.began
-	if c != "foobar" {
-		t.Errorf("The command was not set to %s.", "foobar")
-	}
-	if !reflect.DeepEqual(e, map[string]string{"foo": "bar"}) {
-		t.Errorf("The environment was not set to {\"foo\":\"bar\"}.")
-	}
-}
-
-func TestJobStart(t *testing.T) {
-	j := NewJob()
-	b := NewBashCommand()
-	j.AddCommand(b)
-	b.Prepare("echo foo", map[string]string{})
-	b.Start()
-	b.MonitorState()
-	if b.CmdPtr() == nil {
-		t.Errorf("Start resulted in a nil CmdPtr.")
-	}
-}
-
-func TestMonitorState1(t *testing.T) {
-	j := NewJob()
-	b := NewBashCommand()
-	j.AddCommand(b)
-	b.Prepare("while true; do echo 1; done", map[string]string{})
-	b.Start()
-	b.MonitorState()
-	b.Kill()
-	if !b.Killed() {
-		t.Fail()
-	}
-}
-
-func TestJobWait(t *testing.T) {
-	j := NewJob()
-	b := NewBashCommand()
-	j.AddCommand(b)
-	b.Prepare("echo true", map[string]string{})
-	b.Start()
-	b.MonitorState()
-	b.Wait()
-	if b.Killed() {
-		t.Fail()
-	}
-	if b.ExitCode() == -9000 {
 		t.Fail()
 	}
 }
---
+++ jobs_test.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"os/exec"
+	"reflect"
+	"testing"
+
+	"code.google.com/p/go-uuid/uuid"
+)
+
+func TestJobKilled(t *testing.T) {
+	j := NewBashCommand()
+	if j.Killed() {
+		t.Fail()
+	}
+}
+
+func TestJobSetKilled(t *testing.T) {
+	j := NewBashCommand()
+	j.SetKilled(true)
+	if !j.Killed() {
+		t.Fail()
+	}
+}
+
+func TestGetExitCode(t *testing.T) {
+	j := NewBashCommand()
+	if j.ExitCode() != -9000 {
+		t.Fail()
+	}
+}
+
+func TestSetExitCode(t *testing.T) {
+	j := NewBashCommand()
+	j.SetExitCode(1)
+	if j.ExitCode() != 1 {
+		t.Fail()
+	}
+}
+
+func TestCmdPtr(t *testing.T) {
+	j := NewBashCommand()
+	if j.CmdPtr() != nil {
+		t.Fail()
+	}
+}
+
+func TestSetCmdPtr(t *testing.T) {
+	j := NewBashCommand()
+	c := exec.Command("echo", "true")
+	j.SetCmdPtr(c)
+	if j.CmdPtr() != c {
+		t.Fail()
+	}
+}
+
+func TestUUID(t *testing.T) {
+	j := NewBashCommand()
+	if j.UUID() != "" {
+		t.Fail()
+	}
+}
+
+func TestSetUUID(t *testing.T) {
+	j := NewBashCommand()
+	u := uuid.New()
+	j.SetUUID(u)
+	if j.UUID() != u {
+		t.Fail()
+	}
+}
+
+func TestPrepare(t *testing.T) {
+	j := NewBashCommand()
+	var c string
+	var e map[string]string
+	go func() {
+		c = <-j.command
+		e = <-j.environment
+		<-j.begin
+		j.began <- 1
+	}()
+	j.Prepare("foobar", map[string]string{"foo": "bar"})
+	<-j.began
+	if c != "foobar" {
+		t.Errorf("The command was not set to %s.", "foobar")
+	}
+	if !reflect.DeepEqual(e, map[string]string{"foo": "bar"}) {
+		t.Errorf("The environment was not set to {\"foo\":\"bar\"}.")
+	}
+}
+
+func TestJobStart(t *testing.T) {
+	j := NewJob()
+	b := NewBashCommand()
+	j.AddCommand(b)
+	b.Prepare("echo foo", map[string]string{})
+	b.Start()
+	b.MonitorState()
+	if b.CmdPtr() == nil {
+		t.Errorf("Start resulted in a nil CmdPtr.")
+	}
+}
+
+func TestMonitorState1(t *testing.T) {
+	j := NewJob()
+	b := NewBashCommand()
+	j.AddCommand(b)
+	b.Prepare("while true; do echo 1; done", map[string]string{})
+	b.Start()
+	b.MonitorState()
+	b.Kill()
+	if !b.Killed() {
+		t.Fail()
+	}
+}
+
+func TestJobWait(t *testing.T) {
+	j := NewJob()
+	b := NewBashCommand()
+	j.AddCommand(b)
+	b.Prepare("echo true", map[string]string{})
+	b.Start()
+	b.MonitorState()
+	b.Wait()
+	if b.Killed() {
+		t.Fail()
+	}
+	if b.ExitCode() == -9000 {
+		t.Fail()
+	}
+}
